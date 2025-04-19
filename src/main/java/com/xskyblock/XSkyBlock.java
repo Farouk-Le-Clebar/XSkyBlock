c984aaa58820a812055878a01dfb7a645175ccf3
Louis Huguet <louis.huguet@epitech.eu>
2025-04-19 22:55:02 +0200

    ADD : architecture of the project

---
+++ src/main/java/com/xskyblock/XSkyBlock.java
@@ -0,0 +1,10 @@
+package com.xskyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XSkyBlock extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("XSkyBlock has been enabled!");
+    }
+}
