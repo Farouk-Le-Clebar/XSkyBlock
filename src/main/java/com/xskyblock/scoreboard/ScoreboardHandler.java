4abf8fd04bf09431d48315d1793724df9ba55385
Louis Huguet <louis.huguet@epitech.eu>
2025-04-26 20:16:03 +0200

    adding rank to scoreboard

--- src/main/java/com/xskyblock/scoreboard/ScoreboardHandler.java
+++ src/main/java/com/xskyblock/scoreboard/ScoreboardHandler.java
@@ -29,17 +29,20 @@ public class ScoreboardHandler {
         scoreboard.registerNewTeam("spacer").addEntry("§1");
         scoreboard.registerNewTeam("money").addEntry("§2");
         scoreboard.registerNewTeam("island-level").addEntry("§3");
-        scoreboard.registerNewTeam("players").addEntry("§4");
+        scoreboard.registerNewTeam("actual-rank").addEntry("§4");
+        scoreboard.registerNewTeam("players").addEntry("§5");
 
-        objective.getScore("§1").setScore(4);
-        objective.getScore("§2").setScore(3);
-        objective.getScore("§3").setScore(2);
-        objective.getScore("§4").setScore(1);
+        objective.getScore("§1").setScore(5);
+        objective.getScore("§2").setScore(4);
+        objective.getScore("§3").setScore(3);
+        objective.getScore("§4").setScore(2);
+        objective.getScore("§5").setScore(1);
 
         Bukkit.getScheduler().runTaskTimer(Bukkit.getPluginManager().getPlugin("XSkyBlock"), () -> {
             scoreboard.getTeam("spacer").setPrefix("");
             scoreboard.getTeam("players").setPrefix("§aPlayers: §f" + Bukkit.getOnlinePlayers().size());
             scoreboard.getTeam("money").setPrefix("§aMoney: §f" + userUtils.getMoney(player.getName()) + "$");
+            scoreboard.getTeam("actual-rank").setPrefix("§aRank: §f" + userUtils.getRank(player.getName()));
             scoreboard.getTeam("island-level").setPrefix("§aIsland Level: §f" + configUtils.getIslandLevel(player.getName()));
         }, 0L, 20L);
     }
